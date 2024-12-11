indico/indico

Prevent disclosing if an account exists in Indico

indico/modules/auth/controllers.py
@@ -772,12 +772,23 @@ class RHResetPassword(RH):
             # username than the one he expects. But he still gets back into his profile.
             # Showing a list of usernames would be a little bit more user-friendly but less
             # secure as we'd expose valid usernames for a specific user to an untrusted person.
-            identity = next(iter(user.local_identities))
-            _send_confirmation(form.email.data, 'reset-password', '.resetpass', 'auth/emails/reset_password.txt',
-                               {'user': user, 'username': identity.identifier},
-                               data={'id': identity.id, 'hash': crc32(identity.password_hash)})
+            if user is None:
+                # TODO: Send email informing that the email is not associated to any user?
+                #       We could add in that email a link to create a new account.
+                logger.info('Password reset requested for non-existing user %s', user)
+            elif not user.local_identities:
+                # XXX: Should we allow creating a new identity instead? Would be user-friendly for sure!
+                # TODO: Send email informing that the user has no local account?
+                #       We could add in that email a link to create a new local identity.
+                logger.info('Password reset requested for user %s without local account', user)
+            else:
+                logger.info('Password reset requested for user %s', user)
+                identity = next(iter(user.local_identities))
+                _send_confirmation(form.email.data, 'reset-password', '.resetpass', 'auth/emails/reset_password.txt',
+                                   {'user': user, 'username': identity.identifier},
+                                   data={'id': identity.id, 'hash': crc32(identity.password_hash)})
             session['resetpass_email_sent'] = True
-            logger.info('Password reset requested for user %s', user)
+            # TODO: Should this not return to the login form instead?
             return redirect(url_for('.resetpass'))
         return WPAuth.render_template('reset_password.html', form=form, identity=None, widget_attrs={},
                                       email_sent=session.pop('resetpass_email_sent', False))

UNOG-Indico/UNOG-Indico-v2

indico/modules/auth/controllers.py
@@ -780,12 +780,23 @@ class RHResetPassword(RH):
             # username than the one he expects. But he still gets back into his profile.
             # Showing a list of usernames would be a little bit more user-friendly but less
             # secure as we'd expose valid usernames for a specific user to an untrusted person.
-            identity = next(iter(user.local_identities))
-            _send_confirmation(form.email.data, 'reset-password', '.resetpass', 'auth/emails/reset_password.txt',
-                               {'user': user, 'username': identity.identifier},
-                               data={'id': identity.id, 'hash': crc32(identity.password_hash)})
+            if user is None:
+                # TODO: Send email informing that the email is not associated to any user?
+                #       We could add in that email a link to create a new account.
+                logger.info('Password reset requested for non-existing user %s', user)
+            elif not user.local_identity:
+                # XXX: Should we allow creating a new identity instead? Would be user-friendly for sure!
+                # TODO: Send email informing that the user has no local account?
+                #       We could add in that email a link to create a new local identity.
+                logger.info('Password reset requested for user %s without local account', user)
+            else:
+                logger.info('Password reset requested for user %s', user)
+                identity = user.local_identity
+                _send_confirmation(form.email.data, 'reset-password', '.resetpass', 'auth/emails/reset_password.txt',
+                                   {'user': user, 'username': identity.identifier},
+                                   data={'id': identity.id, 'hash': crc32(identity.password_hash)})
             session['resetpass_email_sent'] = True
-            logger.info('Password reset requested for user %s', user)
+            # TODO: Should this not return to the login form instead?
             return redirect(url_for('.resetpass'))
         return WPAuth.render_template('reset_password.html', form=form, identity=None, widget_attrs={},
                                       email_sent=session.pop('resetpass_email_sent', False))